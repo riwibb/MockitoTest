c74a7ccd17fe505a9d3c5ce4ebb06072cddb940c
baba <baba@Rayn-Keyans-iMac.local>
2017-10-14 14:48:45 -0700

    Iterative Add

--- test/org/rony/MockitoWithRunner.java
+++ test/org/rony/MockitoWithRunner.java
@@ -1,17 +1,12 @@
 package org.rony;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoWithRunner {
@@ -19,13 +14,14 @@ public class MockitoWithRunner {
 	EmployeeDetails mockEmployeeDetails;
 	@Spy
 	EmployeeDetails spy_ed = new EmployeeDetails("Karim", 1200.0, 22);
-	
+
 	@Test
 	public void testMocks() {
 		EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic(mockEmployeeDetails);
 		when(mockEmployeeDetails.getMonthlySalary()).thenReturn(1.0);
 		assertEquals(empBusinessLogic.calculateYearlySalary(), 12, 0);
 	}
+
 	@Test
 	public void testSpy() {
 		doNothing().when(spy_ed).anotherCrapFunction();
@@ -33,6 +29,6 @@ public class MockitoWithRunner {
 		// Calls Real function of ED
 		assertEquals(eb.calculateAppraisal(), 500.0, 0);
 		// Does not call real function of ED
-		eb.callcCrapFunction();		
+		eb.callcCrapFunction();
 	}
 }
